273657d30095a2faecdc72b83647ae68c612cf48
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-04-29 23:49:35 +0000

    poractice components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [counter, setCounter] = useState(0);
+	const [userImput, setUserImput] = useState('');
+
+      function handleClick() {
+		setCounter(counter => counter + 5);
+		
+	  }
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Count: {counter}</h1>
+			<button onClick={() => {
+				handleClick();
+				handleClick();
+				handleClick();
+			}}>
+				Add 15
+			</button>
+			<div>
+				<h1>You name is {userImput ? userImput :  " NULL"}</h1>
+			</div>
+			<div>
+				<input
+				type="text"
+				value={userImput}
+				placeholder="Enter first name"
+				onChange={e => setUserImput(e.target.value)}
+				/>
+			</div>
 		</div>
+
 	);
 };
 
